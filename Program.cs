559a17bc4c4c1d2250baa071ea7f3177b2064817
timberjj <5427963+timberjj@user.noreply.gitee.com>
2020-07-21 01:21:15 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.Azure.ServiceBus;
+namespace servicebus_sender
+{
+    class Program
+    {
+        
+        const string ServiceBusConnectionString = "Endpoint=sb://jimmylab-servicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=DIqHgIjdAQvgRx5UFZm66C0Y8ztM3wXoViuvxRKQs7M=";
+        const string QueueName = "jimmylab-sb1";
+        static IQueueClient client;
+        public static async Task Main(string[] args)
+        {
+            client = new QueueClient(ServiceBusConnectionString, QueueName);
+            var tasks = new List<Task>();
+            for (int i = 0; i < 25000; i++)
+            {
+                string messageBody = $"Message {i}";
+                var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+                tasks.Add(client.SendAsync(message));
+                Console.WriteLine($"Sending message: {messageBody}");
+            }
+            await Task.WhenAll(tasks);
+        }
+    }
+}
